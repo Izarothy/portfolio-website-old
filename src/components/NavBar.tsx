4a0038b65cb6ef391a0a38e89a8c01fbfac944d9
Izarothy <izarothmail@gmail.com>
2022-03-26 21:04:18 +0100

    style: add navbar hover underline

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,14 +11,14 @@ export const NavBar = ({ home, projects, technologies }: Props) => {
   return (
     <nav className="w-full flex justify-end pr-12 text-gray-300 fixed text-base bg-inherit py-3">
       <ul className="flex gap-4 items-center">
-        <li>
+        <li className="list-el">
           <button
             onClick={() => home.current.scrollIntoView({ behavior: 'smooth' })}
           >
             Home
           </button>
         </li>
-        <li>
+        <li className="list-el">
           <button
             onClick={() =>
               technologies.current.scrollIntoView({ behavior: 'smooth' })
@@ -27,7 +27,7 @@ export const NavBar = ({ home, projects, technologies }: Props) => {
             Technologies
           </button>
         </li>
-        <li>
+        <li className="list-el">
           <button
             onClick={() =>
               projects.current.scrollIntoView({ behavior: 'smooth' })
