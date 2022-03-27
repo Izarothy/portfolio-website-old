76ada8b9f95464f1a74c59421d2568267eb469f7
Izarothy <izarothmail@gmail.com>
2022-03-27 10:54:10 +0200

    style: make project overlay more visible

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -17,34 +17,40 @@ export const Project = ({ title, bg, live, github }: Props) => {
       onClick={() => setHovered(!hovered)}
       onKeyDown={() => setHovered(!hovered)}
     >
-      <h3
-        className={`text-center w-full absolute top-20 text-3xl [text-shadow:1px_1px_#111] ${
-          hovered ? `block` : `hidden`
-        }`}
-      >
-        {title}
-      </h3>
       <span
         className={`${
-          hovered ? `block` : `hidden`
-        } absolute bottom-4 flex justify-center w-full gap-4`}
+          hovered ? `bg-black/20` : ``
+        } absolute w-full h-80 transition ease-in-out duration-500`}
       >
-        <a
-          href={live}
-          target="_blank"
-          className="p-2 px-4 hover:bg-white hover:text-primary-dark border border-white rounded-xl"
-          rel="noreferrer"
+        <h3
+          className={`text-center w-full absolute top-6 text-4xl [text-shadow:2px_2px_#111] font-bold ${
+            hovered ? `block` : `hidden`
+          }`}
         >
-          Live version
-        </a>
-        <a
-          href={github}
-          target="_blank"
-          className="p-2 px-4 hover:bg-white hover:text-primary-dark border border-white rounded-xl"
-          rel="noreferrer"
+          {title}
+        </h3>
+        <span
+          className={`${
+            hovered ? `block` : `hidden`
+          } absolute bottom-4 flex justify-center w-full gap-4`}
         >
-          Code
-        </a>
+          <a
+            href={live}
+            target="_blank"
+            className="project-link"
+            rel="noreferrer"
+          >
+            Live version
+          </a>
+          <a
+            href={github}
+            target="_blank"
+            className="project-link"
+            rel="noreferrer"
+          >
+            Code
+          </a>
+        </span>
       </span>
     </article>
   );
